46a39f439c74240cfc3bfd58ac97bc9cf21ac699
SoheilaHashemi <47557034+SoheilaHashemi@users.noreply.github.com>
2023-01-23 11:05:10 -0800

    Add files via upload

---
+++ Swagger/Swagger/Controllers/WeatherForecastSecondryController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Swagger.Controllers
+{
+
+    public class WeatherForecastSecondry : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastSecondry> _logger;
+
+        public WeatherForecastSecondry(ILogger<WeatherForecastSecondry> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+
+    }
+
+}
+
+
